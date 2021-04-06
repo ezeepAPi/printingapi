085561ef09a134aa7788d426f5e39119add08381
mufal <muhammad@ezeep.com>
2021-04-06 15:30:56 +0200

    update ŕeadme.md and printer.py files

--- code-samples-python/README.md
+++ code-samples-python/README.md
@@ -1,1 +1,18 @@
 
+# Python code example for getting access token
+
+To get the access of secure printing  required access token. Python code example to get the access token is given in **oauth.py** 
+
+Getting access token has following workflow
+
+- Get authorization code 
+- Turn the authorization code into a access token
+- Use refresh token to  renew your access token
+
+## Requirements
+
+- client_id
+- client_secret
+- install Python
+- Python IDE (Pycharm,spyder etc)
+
--- code-samples-python/printer.py
+++ code-samples-python/printer.py
@@ -1,28 +1,58 @@
 import requests
 import json
-access_token="put your access token here"
+
+#urls
+access_token="<access token that you get in previous step>"
+refresh_token = "X13RG0nnnrycFYf0sDT8mCFih724VyjZ"
+client_id = "B6zDgZi9YCyIKufRXGt1Q8RD97jsdCQNRWYFf9xK"
 printer_url="https://printapi.ezeep.com/sfapi/GetPrinter/"
 prepare_printer = "https://printapi.ezeep.com/sfapi/PrepareUpload/"
+configuration_url = "https://printapi.ezeep.com/sfapi/GetConfiguration/"
+printer_properties_url="https://printapi.ezeep.com/sfapi/GetPrinterProperties/"
 
-printer_id = requests.get(printer_url,headers={'Authorization': access_token})
+#get configuration of printer
 
+headers={'Authorization': access_token}
+printer_cofiguration =requests.get(configuration_url,headers=headers)
+print(printer_cofiguration.status_code)
+print('body: ' + printer_cofiguration.text)
+
+#get the available printers
+
+printer_id = requests.get(printer_url,headers=headers)
+print("get printer")
+print(printer_id.status_code)
 tokens_id = json.loads(printer_id.text)
 print(tokens_id)
 
-printer_upload = requests.get(prepare_printer,headers={'Authorization': access_token})
+#get the available properties
+printer_properties = requests.get(printer_properties_url + '?id=<token_id from above step>'
+                                                           ,headers={'Authorization': access_token
+                                                                   })
+print("get printer properties")
+tokens_id_1 = json.loads(printer_properties.text)
+print(tokens_id_1)
+
+
+#prepare for file upload in cloud
+printer_upload = requests.get(prepare_printer,headers=headers)
+print("prepare file upload")
 tokens_printer = json.loads(printer_upload.text)
 print(tokens_printer)
 
+#upload a file
 path='C:/Users/mufal/Downloads/week_9.pdf'
 files = {'file': open(path,'rb')}
 file_upload=requests.put(tokens_printer["sasUri"],files=files, headers={"x-ms-blob-type":"BlockBlob","Content-Type":"multipart/form-data"})
+print("file upload")
 print(file_upload)
 
+#print a file
 data={'fileid':tokens_printer['fileid'],'printerid':tokens_id[0]["id"],'type':'pdf'}
 data=json.dumps(data)
 
 print_job=requests.post("https://printapi.ezeep.com/sfapi/Print/",headers={'Authorization': access_token,"Content-Type":"application/json"},data=data)
-print_job_json=json.loads(print_job)
+print_job_json=json.loads(print_job.text)
 
 #To check the print status
 
