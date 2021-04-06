fdc2887867babd901dd6f81dba9b8b2704842e8a
Muhammad Ahmad Falak <wattoorocx@gmail.com>
2021-04-06 13:44:07 +0200

    add refresh token code

--- code-samples-python/oauth.py
+++ code-samples-python/oauth.py
@@ -11,8 +11,8 @@ callback_uri = "https://www.ezeep.com"
 
 
 #client (application) credentials
-client_id = 'put your client id'
-client_secret = "put your client secret"
+client_id = '<put your client_id here>'
+client_secret = "<put your client_secret here>"
 #step A - simulate a request from a browser on the authorize_url - will return an authorization code after the user is
 # prompted for credentials.
 
@@ -23,18 +23,33 @@ print("go to the following url on the browser and enter the code from the return
 print("---  " + authorization_redirect_url + "  ---")
 authorization_code = input('code: ')
 
-# step -turn the authorization code into a access token, etc
+# step -turn the authorization code into a access token
 data = {'grant_type': 'authorization_code', 'code': authorization_code}
 print("requesting access token")
-access_token_response = requests.post(token_url, data=data, verify=False, allow_redirects=False, auth=(client_id, client_secret))
+access_token_response = requests.post(token_url, data=data, auth=(client_id, client_secret))
 
 print("response")
-print(access_token_response.headers)
+print(access_token_response.status_code)
 print('body: ' + access_token_response.text)
 
 # we can now use the access_token as much as we want to access protected resources.
 tokens = json.loads(access_token_response.text)
 access_token = tokens['access_token']
 print("access token: " + access_token)
+
+#now we will get refresh token because access token will expire after 1 hour
+#instead of doing all of the above procedure just use refresh token code to  renew your access token
+
+refresh_code=tokens["refresh_token"]
+
+data = {'grant_type': 'refresh_token', 'refresh_token':refresh_code }
+print("requesting access token")
+refresh_token_response = requests.post(token_url, data=data, auth=(client_id, client_secret))
+
+print("refresh response")
+print(refresh_token_response.status_code)
+print('body: ' + refresh_token_response.text)
+
+
 
 
